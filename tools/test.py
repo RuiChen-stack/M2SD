94efe6d6bc28f4481100a71be049788ffdcad51d
Ipython <89190797+RuiChen-stack@users.noreply.github.com>
2025-01-08 21:20:06 +0800

    Update test.py

--- tools/test.py
+++ tools/test.py
@@ -6,8 +6,6 @@ import torch
 from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
 from mmcv.runner import get_dist_info, init_dist, load_checkpoint
 from mmcv.utils import DictAction
-import sys
-sys.path.append('/mnt/data/experiments/mirror_tcsvt/')
 from mmseg.apis import multi_gpu_test, single_gpu_test
 from mmseg.datasets import build_dataloader, build_dataset
 from mmseg.models import build_segmentor
